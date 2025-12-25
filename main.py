3f6ee0686c281f710bcdbb52e314d3aafa7155b3
Sameer Meher <sam.tiku@gmail.com>
2025-12-25 16:14:00 +0530

    Added Pydantic output parsers

--- main.py
+++ main.py
@@ -8,17 +8,33 @@ from langchain_classic.agents.react.agent import create_react_agent
 #from langchain import hub
 #from langchain.agents import AgentExecutor
 #from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
-
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model_name="gpt-4")
 react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=[
+        "tool_names",
+        "input",
+        "agent_scratchpad",
+    ]
+).partial(
+    format_instructions=output_parser.get_format_instructions()
+)
+
 agent = create_react_agent(
             llm=llm,
             tools=tools,
-            prompt=react_prompt
+            prompt=react_prompt_with_format_instructions
         )
 agent_executor = AgentExecutor(
             agent=agent,
