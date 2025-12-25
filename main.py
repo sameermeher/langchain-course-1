5daa3df0052ee3de86713ac3e83c818525d3934b
Sameer Meher <sam.tiku@gmail.com>
2025-12-25 11:49:30 +0530

    Structured Output

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,10 +10,23 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the agent response"""
+
+    answer: str = Field(description="The agent's answer to the query")  
+    sources: List[Source] = Field(
+                                default_factory=list,
+                                description="The list of sources used by the agent to answer the question")
+
 
 llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course-1!")
