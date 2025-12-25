9e71c6a5312b92cca7e21feb4aead77512f6e150
Sameer Meher <sam.tiku@gmail.com>
2025-12-25 15:41:19 +0530

    Tavily search witj Agent Executor

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+#from langchain import hub
+#from langchain.agents import AgentExecutor
+#from langchain.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model_name="gpt-4")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(
+            llm=llm,
+            tools=tools,
+            prompt=react_prompt
+        )
+agent_executor = AgentExecutor(
+            agent=agent,
+            tools=tools,
+            verbose=True,
+        )
+chain = agent_executor
+
+
 def main():
-    print("Hello from langchain-course-1!")
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details"
+        }
+    )
+    print(result)
 
 
 if __name__ == "__main__":
